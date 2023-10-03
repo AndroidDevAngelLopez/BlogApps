1e8e02938dd7d5dbbbdccdd12919b55e9197a572
Angel Lopez <angelcecyt09@gmail.com>
2023-10-03 14:05:20 -0600

    refactor constatns

--- site/src/jvmMain/kotlin/org/example/blogapp/util/Constants.kt
+++ site/src/jvmMain/kotlin/org/example/blogapp/util/Constants.kt
@@ -1,7 +1,7 @@
 package org.example.blogapp.util
 
 object Constants {
-        const val CONNECTION_STRING_URI_PLACEHOLDER = "mongodb+srv://angelcecyt09:38QFLigkJUvclGFW@mycluster.qswjfcb.mongodb.net/"
+        const val CONNECTION_STRING_URI_PLACEHOLDER = "mongo"
     const val DATABASE_NAME ="MyBlog"
     const val COLLECTION_NAME ="users"
     const val MAIN_POSTS_LIMIT = 4
