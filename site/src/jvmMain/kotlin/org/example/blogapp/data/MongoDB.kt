00e2956083923a6b2a00eb723202a057839c61c6
Angel Lopez <angelcecyt09@gmail.com>
2023-10-29 21:09:45 -0600

    portfolio pre-alpha-0.1 Trinity

--- site/src/jvmMain/kotlin/org/example/blogapp/data/MongoDB.kt
+++ site/src/jvmMain/kotlin/org/example/blogapp/data/MongoDB.kt
@@ -23,9 +23,10 @@ fun initMongoDB(ctx: InitApiContext) {
 }
 
 class MongoDB(private val context: InitApiContext) : MongoRepository {
-
+    private val systemVar = System.getenv("mongo")
+    //MongoClient.create("mongodb+srv://angelcecyt09:38QFLigkJUvclGFW@mycluster.qswjfcb.mongodb.net/?retryWrites=true&w=majority")
     private val client =
-        MongoClient.create("mongodb+srv://angelcecyt09:38QFLigkJUvclGFW@mycluster.qswjfcb.mongodb.net/?retryWrites=true&w=majority")
+        MongoClient.create(systemVar)
     private val database = client.getDatabase(DATABASE_NAME)
     private val userCollection = database.getCollection<User>("user")
     private val postCollection = database.getCollection<Post>("post")
